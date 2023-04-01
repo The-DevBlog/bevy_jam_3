eecc5ac954b895c8830467cb4e1cfd7f8feddf33
andrew <andrewcs149@gmail.com>
2023-04-01 15:47:48 -0700

    Add placeholder health and stamina bar

--- src/game/hud/hud_sys.rs
+++ src/game/hud/hud_sys.rs
@@ -1,9 +1,51 @@
 use bevy::prelude::*;
 
-pub fn spawn_healthbar(mut cmds: Commands, assets: Res<AssetServer>) {
-    // let container = (NodeBundle {
+fn txt(txt: &str, name: &str, assets: &Res<AssetServer>) -> (TextBundle, Name) {
+    (
+        TextBundle::from_section(
+            txt.to_string(),
+            TextStyle {
+                color: Color::WHITE,
+                font: assets.load("fonts/FiraSans-Bold.ttf"),
+                font_size: 25.0,
+            },
+        ),
+        Name::new(name.to_string()),
+    )
+}
 
-    // })
+fn container(color: Color, position_left: Val, name: &str) -> (NodeBundle, Name) {
+    (
+        NodeBundle {
+            background_color: color.into(),
+            style: Style {
+                align_self: AlignSelf::FlexStart,
+                align_items: AlignItems::Center,
+                padding: UiRect::all(Val::Px(20.0)),
+                position: UiRect::left(position_left),
+                size: Size::height(Val::Px(45.0)),
+                ..default()
+            },
+            ..default()
+        },
+        Name::new(name.to_string()),
+    )
+}
 
-    let txt = (TextBundle::from_section)
+pub fn spawn_health_bar(mut cmds: Commands, assets: Res<AssetServer>) {
+    let container = container(Color::RED, Val::Percent(0.0), "Health Bar");
+    let txt = txt("HEALTH", "Health Bar Text", &assets);
+
+    cmds.spawn(container).with_children(|parent| {
+        parent.spawn(txt);
+    });
+}
+
+pub fn spawn_stamina_bar(mut cmds: Commands, assets: Res<AssetServer>) {
+    let container = container(Color::DARK_GREEN, Val::Percent(81.5), "Stamina Bar");
+    let txt = txt("STAMINA", "Stamina Bar Text", &assets);
+
+    cmds.spawn(container).with_children(|parent| {
+        parent.spawn(txt);
+    });
 }
--- src/game/hud/mod.rs
+++ src/game/hud/mod.rs
@@ -1,7 +1,13 @@
 use bevy::prelude::*;
 
+mod hud_sys;
+
+use hud_sys::*;
+
 pub struct HudPlugin;
 
 impl Plugin for HudPlugin {
-    fn build(&self, app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        app.add_startup_systems((spawn_health_bar, spawn_stamina_bar));
+    }
 }
